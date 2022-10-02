2518b22321caff940c8031f35e51e7525443ddd4
ivan <kulikovia.72@icloud.com>
2022-10-03 00:05:19 +0300

    implement AssetsPlugin

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "Plugins",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .plugin(
+            name: "AssetsPlugin",
+            targets: [
+                "AssetsPlugin"
+            ]
+        )
+    ],
+    targets: [
+        .executableTarget(name: "AssetsPluginTool"),
+        .plugin(
+            name: "AssetsPlugin",
+            capability: .buildTool(),
+            dependencies: [
+                .target(name: "AssetsPluginTool")
+            ]
+        )
+    ]
+)
---
+++ Plugins/AssetsPlugin/plugin.swift
@@ -0,0 +1,28 @@
+import PackagePlugin
+
+@main
+struct AssetsPlugin: BuildToolPlugin {
+    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        guard let target = target as? SourceModuleTarget else { return [] }
+
+        return try target
+            .sourceFiles(withSuffix: "xcassets")
+            .map { try buildCommand(for: $0, context: context) }
+    }
+
+    private func buildCommand(for assetCatalog: File, context: PluginContext) throws -> Command {
+        let assetCatalogPath = assetCatalog.path
+        let assetCatalogName = assetCatalogPath.lastComponent
+        let generatedFileName = assetCatalogPath.stem + ".swift"
+        let generatedFilePath = context.pluginWorkDirectory.appending(generatedFileName)
+        let executablePath = try context.tool(named: "AssetsTool").path
+
+        return .buildCommand(
+            displayName: "Generating \(generatedFileName) for \(assetCatalogName)",
+            executable: executablePath,
+            arguments: [assetCatalogPath, generatedFilePath],
+            inputFiles: [assetCatalogPath],
+            outputFiles: [generatedFilePath]
+        )
+    }
+}
